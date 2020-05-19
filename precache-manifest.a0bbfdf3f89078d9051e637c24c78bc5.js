4f493bd02486f502e87a5f84851d2386b4575125
Roxane Haddad <roxane.haddad@gmail.com>
2020-05-19 13:28:15 +0200

    Updates

---
+++ precache-manifest.a0bbfdf3f89078d9051e637c24c78bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bcac1e868b51936cdef2e9e22f26da",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "0de0c7b07799eda426ac",
+    "url": "/todolistreact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0de0c7b07799eda426ac",
+    "url": "/todolistreact/static/js/2.3e4bace3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.3e4bace3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b8e8dd69c36d40110a",
+    "url": "/todolistreact/static/js/main.bfef6b1a.chunk.js"
+  },
+  {
+    "revision": "3584cb344911f1622137",
+    "url": "/todolistreact/static/js/runtime-main.189efecd.js"
+  }
+]);
